4c61a290f36f4af6b3290ef471addd40d93a0ea6
Keddins <eddinskeathan@gmail.com>
2023-10-30 16:43:12 -0700

    taxicab.py

--- taxicab.py
+++ taxicab.py
@@ -22,9 +22,3 @@ class Taxicab:
         self.__odometer += abs(distance)
 
 
-cab = Taxicab(5, -8)       # creates a Taxicab object at coordinates (5, -8)
-cab.move_x(3)              # moves cab 3 units "right"
-cab.move_y(-4)             # moves cab 4 units "down"
-cab.move_x(-1)             # moves cab 1 unit "left"
-print(cab.get_odometer())  # prints the current odometer reading
-# At this point the cab has traveled 3 + 4 + 1 = 8 units and is now at coordinates (7, -12)
